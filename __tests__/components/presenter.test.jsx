0876564fed072800693c547f5e08280c22044935
Eric Butler <ericbutler000@gmail.com>
2017-05-07 17:50:33 -0400

    Added test around the presenters handling of clickc events

--- __tests__/components/presenter.test.jsx
+++ __tests__/components/presenter.test.jsx
@@ -1,15 +1,22 @@
-import Presenter from '../../src/components/presenter.jsx';
+import Presenter from "../../src/components/presenter"
+import ReactTestUtils from "react-dom/test-utils"
 
-describe('Presenter component', () => {
-	let stubbedMessage = 'some bullshit'
+describe("Presenter component", () => {
+	let stubbedMessage = "some bullshit"
+	let clickSpy = jest.fn()
 
 	beforeEach(() => {
-		document.body.appendChild(document.createElement('main'))
-		ReactDOM.render(<Presenter text={ stubbedMessage } />, $('main'))
+		document.body.appendChild(document.createElement("main"))
+		ReactDOM.render(<Presenter text={stubbedMessage} clickEvent={clickSpy} />, $("main"))
 	})
 
-	test('renders the message', () => {
-		expect($$('h1').length).toBe(1)
-		expect($('h1').innerHTML).toEqual(stubbedMessage)
+	test("renders the message", () => {
+		expect($$("h1").length).toBe(1)
+		expect($("h1").innerHTML).toEqual(stubbedMessage)
+	})
+
+	test("fires the handler on click", () => {
+		ReactTestUtils.Simulate.click($("button"))
+		expect(clickSpy.mock.calls.length).toBe(1)
 	})
 })
